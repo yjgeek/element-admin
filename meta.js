e896b9f43d1f908ed7289e701edc3d68502b64b7
unknown <1668171322@qq.com>
2018-06-28 21:50:32 +0800

    修复路由配置出错

--- meta.js
+++ meta.js
@@ -69,11 +69,6 @@ module.exports = {
         },
       ],
     },
-    router: {
-      when: 'isNotTest',
-      type: 'confirm',
-      message: 'Install vue-router?',
-    },
     lint: {
       when: 'isNotTest',
       type: 'confirm',
@@ -168,8 +163,7 @@ module.exports = {
     'test/unit/karma.conf.js': "unit && runner === 'karma'",
     'test/unit/specs/index.js': "unit && runner === 'karma'",
     'test/unit/setup.js': "unit && runner === 'jest'",
-    'test/e2e/**/*': 'e2e',
-    'src/router/**/*': 'router',
+    'test/e2e/**/*': 'e2e'
   },
   complete: function(data, { chalk }) {
     const green = chalk.green
